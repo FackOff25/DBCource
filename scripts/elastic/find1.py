132ff5835c037d6824f4282c29ba9a16873f034f
fackoff <fackoff@debian.debian>
2025-06-08 22:20:43 +0300

    elastic find1

---
+++ scripts/elastic/find1.py
@@ -0,0 +1,38 @@
+#! /bin/python3
+import json
+from elasticsearch import Elasticsearch
+import pprint
+
+# Параметры соединения с ES
+client = Elasticsearch("http://localhost:9200")
+
+# Индексы
+rooms_id = "index_room"
+clients_id = "clients"
+
+searchBody = {
+  "size": 0,
+  "aggs": {
+    "arrival_by_year": {
+      "date_histogram": {
+        "field": "arriving_date",
+        "calendar_interval": "1y",
+      },
+      "aggs": {
+        "rooms_aggregation": {
+          "terms": {
+            "field": "room_id",
+            "size": 10
+          }
+        }
+      }
+    }
+  }
+}
+
+result = client.search(index=clients_id, body=searchBody)
+
+result_str = json.dumps(result, indent=4, sort_keys=True)
+with open("../results/1.json", "w+") as resultfile:
+    resultfile.write(result_str)
+pprint.pprint(result_str)
