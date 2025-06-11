763ac87d7a446b22154908dcf1e984eda4413731
fackoff <fackoff@debian.debian>
2025-06-11 10:46:51 +0300

    uncommented stuff

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 source ./settings.sh
 sudo docker-compose down
 sudo docker-compose up -d --build
-#sudo docker exec -i -t $CONTAINER_ELASTIC chmod -R g+w,o+w /scripts/
-#sudo docker exec -i -t $CONTAINER_KIBANA systemctl start kibana.service
-#sudo docker exec -i -t $CONTAINER_NEO4J /bin/sh -c "/usr/bin/neo4j-admin dbms set-initial-password iu6-magisters"
-#sudo docker exec -i -t $CONTAINER_NEO4J service neo4j start
-#sudo docker exec -i -t $CONTAINER_SPARK service ssh restart
+sudo docker exec -i -t $CONTAINER_ELASTIC chmod -R g+w,o+w /scripts/
+sudo docker exec -i -t $CONTAINER_KIBANA systemctl start kibana.service
+sudo docker exec -i -t $CONTAINER_NEO4J /bin/sh -c "/usr/bin/neo4j-admin dbms set-initial-password iu6-magisters"
+sudo docker exec -i -t $CONTAINER_NEO4J service neo4j start
+sudo docker exec -i -t $CONTAINER_SPARK service ssh restart
 sudo docker exec -i -t $CONTAINER_PGVECTOR chown -R postgres:postgres /var/lib/postgresql/16/main
 sudo docker exec -i -t $CONTAINER_PGVECTOR service postgresql restart
 sudo docker exec -i -t $CONTAINER_PGVECTOR chmod -R g+w,o+w /scripts/
